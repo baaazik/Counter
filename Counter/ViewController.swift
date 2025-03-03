ddc9657203d7e2f6206b5d22c0eddfefc344f6f8
bazik <zabaznova2303@gmail.com>
2025-03-03 23:45:31 +0300

    Создан проект

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Анжелика Забазнова on 03.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
